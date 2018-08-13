595398895e10221315a432afae5c7f952e678b22
Unknown <vfariachavez@gmail.com>
2018-08-13 17:30:31 +0100

    barra navegacion

--- 3. App interna /src/app/app.module.ts
+++ 3. App interna /src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProveedoresComponent } from './componentes/proveedores/proveedores.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'proveedores', component: ProveedoresComponent},
+  {path: '**', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent,
+    NavegacionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
