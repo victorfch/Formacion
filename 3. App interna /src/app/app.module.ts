58ea12c6ed1c7a8128ba0577687927c75264dfbb
Unknown <vfariachavez@gmail.com>
2018-08-13 16:57:23 +0100

    componente proveedores y servicio

--- 3. App interna /src/app/app.module.ts
+++ 3. App interna /src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProveedoresComponent } from './componentes/proveedores/proveedores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule
