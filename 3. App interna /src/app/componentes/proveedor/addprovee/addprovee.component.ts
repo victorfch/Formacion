f6a8a2b09edfc932d419566c05d4c44396f7b637
Unknown <vfariachavez@gmail.com>
2018-08-13 19:35:40 +0100

    jugando con validadores

--- 3. App interna /src/app/componentes/proveedor/addprovee/addprovee.component.ts
+++ 3. App interna /src/app/componentes/proveedor/addprovee/addprovee.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-addprovee',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproveeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formpro') formpro: NgForm;
+  proveedor:any;
+  provincias: string[] = [ 'Álava','Albacete','Alicante','Almería','Asturias','Ávila','Badajoz',
+     'Barcelona','Burgos', 'Cáceres', 'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba',
+     'La Coruña','Cuenca','Gerona','Granada','Guadalajara', 'Guipúzcoa','Huelva','Huesca',
+     'IslasBaleares','Jaén','León','Lérida','Lugo','Madrid', 'Málaga','Murcia','Navarra','Orense',
+     'Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria',
+     'Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya',
+     'Zamora','Zaragoza' ];
+
+  constructor() {
+    this.proveedor = {
+      nombre:'',
+      cif:'',
+      direccion:'',
+      cp:'',
+      localidad:'',
+      provincia:'',
+      telefono:null,
+      email:'',
+      contacto:''
+    };
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.proveedor.nombre=this.formpro.value.nombre;
+    this.proveedor.cif=this.formpro.value.cif;
+    this.proveedor.direccion=this.formpro.value.direccion;
+    this.proveedor.cp=this.formpro.value.cp;
+    this.proveedor.localidad=this.formpro.value.localidad;
+    this.proveedor.provincia=this.formpro.value.provincia;
+    this.proveedor.telefono=this.formpro.value.telefono;
+    this.proveedor.email=this.formpro.value.email;
+    this.proveedor.contacto=this.formpro.value.contacto;
+
+    this.formpro.reset();
   }
 
 }
