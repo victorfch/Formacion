8bbef643a297b88c786581997a49521e4dceb204
Unknown <vfariachavez@gmail.com>
2018-08-14 22:15:58 +0100

    logica formulario presupuesto

--- 3. App interna /src/app/componentes/presupuestos/addpres/addpres.component.ts
+++ 3. App interna /src/app/componentes/presupuestos/addpres/addpres.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-addpres',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addpres.component.css']
 })
 export class AddpresComponent implements OnInit {
+  presupuestoForm: FormGroup;
+  presupuesto:any;
 
-  constructor() { }
+
+
+  constructor(private pf: FormBuilder) { }
 
   ngOnInit() {
+    this.presupuestoForm=this.pf.group({
+      proveedor:'',
+      fecha:'',
+      concepto:'',
+      base:'',
+      tipo:'',
+      iva:'',
+      total:''
+    });
+  }
+
+  savePresupuesto(){
+    const savePresupuesto={
+      proveedor: this.presupuestoForm.get('proveedor').value,
+      fecha: this.presupuestoForm.get('fecha').value,
+      concepto: this.presupuestoForm.get('concepto').value,
+      base: this.presupuestoForm.get('base').value,
+      tipo: this.presupuestoForm.get('tipo').value,
+      iva: this.presupuestoForm.get('iva').value,
+      total: this.presupuestoForm.get('total').value
+    }
+    return savePresupuesto;
+  }
+
+  onSubmit(){
+    this.presupuesto=this.savePresupuesto();
   }
 
 }
